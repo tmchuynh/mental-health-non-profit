1a40cd43855332c71a1bbfe748a7b71a4a074016
Tina Huynh <tinamchuynh@gmail.com>
2025-05-20 12:06:41 -0700

    Adds crisis helpline directory for enhanced support resources

---
+++ src/lib/constants/crisisHelplines.ts
@@ -0,0 +1,60 @@
+import { HelplineEntry } from "../interfaces/info";
+
+export const helplineDirectory: HelplineEntry[] = [
+  {
+    name: "National Suicide Prevention Lifeline (USA)",
+    phone: "988",
+    website: "https://988lifeline.org",
+    description:
+      "Provides 24/7 free and confidential support for people in distress, prevention and crisis resources for you or your loved ones.",
+    availability: "24/7",
+  },
+  {
+    name: "Crisis Text Line (USA)",
+    phone: "Text HOME to 741741",
+    website: "https://www.crisistextline.org",
+    description:
+      "Free, 24/7 support via text message for those in crisis. Trained crisis counselors provide confidential support.",
+    availability: "24/7",
+  },
+  {
+    name: "SAMHSA’s National Helpline (Substance Abuse and Mental Health Services Administration)",
+    phone: "1-800-662-HELP (4357)",
+    website: "https://www.samhsa.gov/find-help/national-helpline",
+    description:
+      "Confidential, free, 24/7 treatment referral and information service for individuals facing mental health and substance use disorders.",
+    availability: "24/7",
+  },
+  {
+    name: "The Trevor Project",
+    phone: "1-866-488-7386",
+    website: "https://www.thetrevorproject.org",
+    description:
+      "Provides crisis intervention and suicide prevention services to LGBTQ+ youth, available 24/7 via phone, chat, and text.",
+    availability: "24/7",
+  },
+  {
+    name: "Veterans Crisis Line (USA)",
+    phone: "988 then Press 1",
+    website: "https://www.veteranscrisisline.net",
+    description:
+      "Confidential support for veterans and their families. Available 24/7 by phone, text, or online chat.",
+    availability: "24/7",
+  },
+  {
+    name: "Crisis Services Canada",
+    phone: "1-833-456-4566",
+    website: "https://www.crisisservicescanada.ca/en/",
+    description:
+      "Offers 24/7 crisis support via phone and text for Canadians experiencing emotional distress or suicidal thoughts.",
+    availability: "24/7",
+  },
+  {
+    name: "Lifeline Australia",
+    phone: "13 11 14",
+    website: "https://www.lifeline.org.au",
+    description:
+      "Provides 24/7 crisis support and suicide prevention services for people in Australia via phone and online chat.",
+    availability: "24/7",
+  },
+];
