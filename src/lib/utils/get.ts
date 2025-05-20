56c285cfb7cc6556c1c128b43a9314fa9d305176
Tina Huynh <tinamchuynh@gmail.com>
2025-05-20 12:50:29 -0700

    Renames toolModule to articleModule for clarity in getArticleData function

--- src/lib/utils/get.ts
+++ src/lib/utils/get.ts
@@ -3,14 +3,35 @@ export async function getArticleData(
   article: string
 ): Promise<any> {
   try {
-    const toolModule = await import(
+    const articleModule = await import(
       `@/lib/constants/articles/${articleCategory}`
     );
     // Return the specific named export that matches toolKitID
-    if (toolModule[article]) {
-      return toolModule[article];
+    if (articleModule[article]) {
+      return articleModule[article];
     } else {
       console.error(`Export named ${article} not found in module`);
+      return [];
+    }
+  } catch (error) {
+    console.error(`Error loading resource: ${error}`);
+    return [];
+  }
+}
+
+export async function getResourceData(
+  resourceCategory: string,
+  resource: string
+): Promise<any> {
+  try {
+    const resourceModule = await import(
+      `@/lib/constants/resources/${resourceCategory}`
+    );
+    // Return the specific named export that matches toolKitID
+    if (resourceModule[resource]) {
+      return resourceModule[resource];
+    } else {
+      console.error(`Export named ${resource} not found in module`);
       return [];
     }
   } catch (error) {
