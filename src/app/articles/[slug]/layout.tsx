2f5bb3d58a9dc2c12a7d1d4fc51577fd20c44fa8
Tina Huynh <tinamchuynh@gmail.com>
2025-05-19 20:12:59 -0700

    Adds layout component for article detail pages
    Implements a new layout component that provides context for article detail pages.
    
    It integrates the ArticleProvider to manage article-related state and displays the article title and description dynamically based on the URL path.
    
    This enhances the user experience by ensuring relevant information is presented for each article.

---
+++ src/app/articles/[slug]/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { ArticleProvider } from "@/context/ArticleContext";
+import { articlesMap } from "@/lib/constants/articleSectioned";
+import { capitalize, formatUrlToID } from "@/lib/utils/format";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  const article = usePathname().split("/").slice(-1)[0];
+  const articleCategory = usePathname().split("/").slice(-2)[0];
+
+  const toolKitID = formatUrlToID(articleCategory);
+  const toolName = capitalize(article);
+
+  const correspondingTools = articlesMap[toolKitID as keyof typeof articlesMap];
+
+  const articleInfo = correspondingTools?.find(
+    (toolInfo) => toolInfo.title === toolName
+  );
+
+  return (
+    <ArticleProvider value={{ article, articleCategory, articleInfo }}>
+      <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-10/12 md:w-11/12">
+        <h1>{capitalize(article)}</h1>
+        <h5>{articleInfo?.description}</h5>
+        {children}
+      </div>
+    </ArticleProvider>
+  );
+}
