6a58965a92703679ac538648f289d42492429c61
Tina Huynh <tinamchuynh@gmail.com>
2025-05-19 21:42:33 -0700

    feat: enhance MenuBarNav with router integration for improved navigation

--- src/components/navigation/MenuBarNav.tsx
+++ src/components/navigation/MenuBarNav.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Menubar,
   MenubarCheckboxItem,
@@ -12,9 +14,11 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import { menuData } from "@/lib/constants/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { useRouter } from "next/navigation";
 import { ThemeToggle } from "../buttons/ThemeToggle";
 
-function renderMenuItems(items: any[]) {
+function renderMenuItems(items: any[], router: any[] | AppRouterInstance) {
   return items.map((item, idx) => {
     if (item.type === "separator") return <MenubarSeparator key={idx} />;
     if (item.type === "checkbox")
@@ -27,12 +31,19 @@ function renderMenuItems(items: any[]) {
       return (
         <MenubarSub key={idx}>
           <MenubarSubTrigger>{item.label}</MenubarSubTrigger>
-          <MenubarSubContent>{renderMenuItems(item.items)}</MenubarSubContent>
+          <MenubarSubContent>
+            {renderMenuItems(item.items, router)}
+          </MenubarSubContent>
         </MenubarSub>
       );
     // Default: regular item
     return (
-      <MenubarItem key={idx} disabled={item.disabled} inset={item.inset}>
+      <MenubarItem
+        key={idx}
+        disabled={item.disabled}
+        inset={item.inset}
+        onClick={() => router.push(item.href)}
+      >
         {item.label}
         {item.shortcut && <MenubarShortcut>{item.shortcut}</MenubarShortcut>}
       </MenubarItem>
@@ -41,13 +52,16 @@ function renderMenuItems(items: any[]) {
 }
 
 export function MenuBarNav() {
+  const router = useRouter();
   return (
-    <Menubar className="flex justify-between items-center mx-auto my-4 py-7 w-10/12 md:w-11/12">
+    <Menubar className="flex justify-between items-center mx-auto my-4 w-10/12 md:w-11/12">
       <div className="flex gap-2">
         {menuData.map((menu, idx) => (
           <MenubarMenu key={idx}>
             <MenubarTrigger>{menu.label}</MenubarTrigger>
-            <MenubarContent>{renderMenuItems(menu.items)}</MenubarContent>
+            <MenubarContent>
+              {renderMenuItems(menu.items, router)}
+            </MenubarContent>
           </MenubarMenu>
         ))}
       </div>
