458ebbd129a143e1e10166249a40b1b6ce50d778
Tina Huynh <tinamchuynh@gmail.com>
2025-05-19 23:54:11 -0700

    Adds impact stories section to testimonials page
    Incorporates the `ImpactStoryCard` component to display
    impact stories on the testimonials page. This enhances
    the user experience by showcasing community feedback
    and inspiring narratives, aligning with the mission
    to highlight positive impacts.

--- src/app/programs-and-services/testimonials/page.tsx
+++ src/app/programs-and-services/testimonials/page.tsx
@@ -1,3 +1,6 @@
+import { ImpactStoryCard } from "@/components/ImpactStoryCard";
+import { impactStories } from "@/lib/constants/about/story";
+
 export default function Testimonials() {
   return (
     <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-10/12 md:w-11/12">
@@ -7,6 +10,11 @@ export default function Testimonials() {
         firsthand. Their stories inspire us to continue our mission and make a
         difference in the community.
       </p>
+      <div className="mt-8">
+        {impactStories.map((story, idx) => (
+          <ImpactStoryCard key={idx} story={story} />
+        ))}
+      </div>
     </div>
   );
 }
