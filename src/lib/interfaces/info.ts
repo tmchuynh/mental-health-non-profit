65190e3c8d969c227ba1f7865ed73930dfb2f3d2
Tina Huynh <tinamchuynh@gmail.com>
2025-05-19 21:43:33 -0700

    Adds interfaces for IconBullet, FeaturedDonor, ImpactStory, KeyStatistic, CompanyMilestone, StaffMember, and StaffCategory

---
+++ src/lib/interfaces/info.ts
@@ -0,0 +1,48 @@
+import { IconType } from "react-icons/lib";
+
+export interface IconBullet {
+  icon: IconType;
+  title: string;
+  description: string;
+}
+
+export interface FeaturedDonor {
+  name: string;
+  quote: string;
+  representative: string;
+  type: "Foundation" | "Corporate" | "Trust" | "Individual";
+}
+
+export interface ImpactStory {
+  title: string;
+  subtitle: string;
+  background: string;
+  role: string;
+  outcome: string[];
+  quote: string;
+}
+
+export interface KeyStatistic {
+  title: string;
+  value: string;
+  description?: string;
+}
+
+export interface CompanyMilestone {
+  year: number;
+  title: string;
+  highlights: string[];
+}
+
+export interface StaffMember {
+  name: string;
+  title: string;
+  bio: string;
+  email: string;
+  image: string;
+}
+
+export interface StaffCategory {
+  category: string;
+  team: StaffMember[];
+}
