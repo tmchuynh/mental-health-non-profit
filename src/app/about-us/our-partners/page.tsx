9ec2c00560548312aa6ad50fc627d99eb6bc97f5
Tina Huynh <tinamchuynh@gmail.com>
2025-05-19 23:53:44 -0700

    Adds partner categorization and improved description
    Enhances the "Our Partners" section by introducing
    a categorized display of partners.
    
    Updates the text to highlight the importance of
    partners in delivering quality care and making a
    positive community impact.

--- src/app/about-us/our-partners/page.tsx
+++ src/app/about-us/our-partners/page.tsx
@@ -1,3 +1,6 @@
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+import { categorizedPartners } from "@/lib/constants/about/partners";
+
 export default function OurPartners() {
   return (
     <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-10/12 md:w-11/12">
@@ -5,8 +8,28 @@ export default function OurPartners() {
       <p>
         We are proud to collaborate with a variety of organizations and
         individuals who share our commitment to making a positive impact in the
-        community.
+        community. Without the support of our partners, we would not be able to
+        do what we do. Their contributions help us to provide the best possible
+        care to our clients and to make a positive impact in the community.
       </p>
+
+      <section>
+        {categorizedPartners.map((category, index) => (
+          <div key={index}>
+            <h2>{category.category}</h2>
+            <div className="gap-4 grid md:grid-cols-2 lg:grid-cols-3 my-5">
+              {category.partners.map((partner) => (
+                <Card key={partner.name}>
+                  <CardHeader>{partner.description}</CardHeader>
+                  <CardFooter>
+                    <h4>{partner.name}</h4>
+                  </CardFooter>
+                </Card>
+              ))}
+            </div>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
