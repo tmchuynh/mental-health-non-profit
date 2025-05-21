dc9e054be49ef4df5c33986ccde2d030f415da7c
Tina Huynh <tinamchuynh@gmail.com>
2025-05-20 22:32:31 -0700

    Add ADHD resource list with links to support and information

---
+++ src/lib/constants/resources/adhd.ts
@@ -0,0 +1,54 @@
+import { ListDetail } from "@/lib/interfaces/articles";
+
+export const adhd: ListDetail[] = [
+  {
+    title:
+      "Children and Adults with Attention-Deficit/Hyperactivity Disorder (CHADD)",
+    url: "https://chadd.org/",
+  },
+  {
+    title: "National Institute of Mental Health: ADHD",
+    url: "https://www.nimh.nih.gov/health/topics/attention-deficit-hyperactivity-disorder-adhd",
+  },
+  {
+    title: "CDC: What is ADHD?",
+    url: "https://www.cdc.gov/ncbddd/adhd/facts.html",
+  },
+  {
+    title: "Mayo Clinic: ADHD Overview",
+    url: "https://www.mayoclinic.org/diseases-conditions/adhd/symptoms-causes/syc-20350889",
+  },
+  {
+    title: "ADDitude Magazine: ADHD Resources",
+    url: "https://www.additudemag.com/",
+  },
+  {
+    title: "Understood: ADHD Resources for Families",
+    url: "https://www.understood.org/en/articles/adhd-in-children",
+  },
+  {
+    title: "PubMed: ADHD Research",
+    url: "https://pubmed.ncbi.nlm.nih.gov/?term=adhd",
+  },
+  {
+    title:
+      "Books: Driven to Distraction by Edward M. Hallowell, M.D. & John J. Ratey, M.D.",
+    url: "https://www.goodreads.com/book/show/46342.Driven_to_Distraction",
+  },
+  {
+    title: "Books: Taking Charge of ADHD by Russell A. Barkley, PhD",
+    url: "https://www.goodreads.com/book/show/108282.Taking_Charge_of_ADHD",
+  },
+  {
+    title: "Books: Smart but Scattered by Peg Dawson & Richard Guare",
+    url: "https://www.goodreads.com/book/show/1945752.Smart_but_Scattered",
+  },
+  {
+    title: "Reddit: r/ADHD Community",
+    url: "https://www.reddit.com/r/ADHD/",
+  },
+  {
+    title: "7 Cups: Free, anonymous online chat with trained listeners",
+    url: "https://www.7cups.com/",
+  },
+];
