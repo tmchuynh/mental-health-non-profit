f401649ca0e9496b10349935abfe05a9777c8ef2
Tina Huynh <tinamchuynh@gmail.com>
2025-05-21 20:52:37 -0700

    Refactors ArticleCard to support theme-based button styles
    Implements client-side rendering for the ArticleCard component to enhance user experience with theme-aware button styles.
    
    Replaces the Link component with a button that adapts its appearance based on the current theme. Ensures the component only renders after being mounted to avoid client-side rendering issues.
    
    Improves accessibility and usability by providing a more visually consistent interaction method for reading articles.

--- src/components/cards/ArticleCard.tsx
+++ src/components/cards/ArticleCard.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { formatCamelCaseToTitle, formatIDToUrl } from "@/lib/utils/format";
-import Link from "next/link";
+import { useTheme } from "next-themes";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
 
 export function ArticleCard({
   categoryId,
@@ -10,19 +15,29 @@ export function ArticleCard({
   title: string;
   description: string;
 }) {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) return null;
+  // Use the mounted state to determine if the theme is dark or light
+
   return (
     <article className="py-2 max-w-xl">
-      <div className="mb-4 pl-4 border-l font-mono text-gray-400 text-sm">
+      <div className="mb-4 pl-4 border-l font-mono text-sm">
         {formatCamelCaseToTitle(categoryId)}
       </div>
       <h2 className="mb-2 font-bold font-mono text-lg">{title}</h2>
-      <p className="mb-6 font-mono text-base text-gray-700">{description}</p>
-      <Link
-        href={`/articles/${formatIDToUrl(title)}`}
-        className="flex items-center gap-1 font-mono text-primary hover:underline"
+      <p className="mb-6 font-mono text-base">{description}</p>
+      <Button
+        variant={theme === "dark" ? "outline" : "link"}
+        onClick={() => router.push(`/articles/${formatIDToUrl(title)}`)}
       >
         Read article
-      </Link>
+      </Button>
     </article>
   );
 }
